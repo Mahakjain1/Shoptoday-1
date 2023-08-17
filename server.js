e09e8f586c8204130051b620c62afc4a919ba883
unknown <mahakj064@gmail.com>
2023-08-17 14:43:00 +0530

    third commit

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import ProductRoutes from "./routes/ProductRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+//esmodule
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
